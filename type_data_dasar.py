4c70c27cdece9a59b228f2fe3a2224f8c2f2c394
Muna 'Adilah <93821740+munaadilah@users.noreply.github.com>
2021-11-06 21:58:07 +0700

    Add files via upload

---
+++ type_data_dasar.py
@@ -0,0 +1,7 @@
+print("Menghitung Luas Lingkaran")
+print("phi = 3.14")
+r = int(input("Masukkan jari-jari lingkaran: "))
+phi = 3.14
+luas = phi*r*r
+
+print("Luas Lingkaran adalah:", luas)
---
+++ typedata.py
@@ -0,0 +1,6 @@
+print("Menghitung Luas Segitiga")
+a = int(input("Masukkan Alas Segitiga: "))
+t = int(input("Masukkan Tinggi Segitiga: "))
+luas = a*t/2
+
+print("Luas Segitiga adalah:", luas)
